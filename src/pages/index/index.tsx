ecaae6a8d8a1554330d8adca5765b8ba7043ae3d
wengweijian <sniggiw@qq.com>
2024-10-10 18:38:26 +0800

    feat:nav tabs fixed effect

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,170 +1,216 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
-  View,
-  Image,
-  Swiper,
-  SwiperItem,
-  ScrollView,
+    View,
+    Image,
+    Swiper,
+    SwiperItem,
+    ScrollView,
 } from "@tarojs/components";
-import { useLoad } from "@tarojs/taro";
+import { useLoad, createSelectorQuery } from "@tarojs/taro";
+import classnames from "classnames";
 import "./index.scss";
 
 import { Random } from "mockjs";
 
-const CARD_TYPE = [
-  "全部",
-  "篮球卡",
-  "足球卡",
-  "宝可梦",
-  "迪士尼",
-  "潮玩卡",
-  "女神卡",
-  "海贼王",
-  "游戏王",
+const NAV_TABS = [
+    "全部",
+    "篮球卡",
+    "足球卡",
+    "宝可梦",
+    "迪士尼",
+    "潮玩卡",
+    "女神卡",
+    "海贼王",
+    "游戏王",
 ];
 
 export default function Index() {
-  useLoad(() => {
-    console.log("Page loaded.");
-  });
+    useLoad(() => {
+        console.log("Page loaded.");
+    });
 
-  const [sort, setSort] = useState("");
+    const [sort, setSort] = useState("");
+    const handleSort = () => {
+        if (sort === "") {
+            setSort("asc");
+        }
 
-  const handleSort = () => {
-    if (sort === "") {
-      setSort("asc");
-    }
+        if (sort === "asc") {
+            setSort("desc");
+        }
 
-    if (sort === "asc") {
-      setSort("desc");
-    }
+        if (sort === "desc") {
+            setSort("");
+        }
+    };
 
-    if (sort === "desc") {
-      setSort("");
-    }
-  };
+    const [navTabFixed, setNavTabFixed] = useState(false);
+    const [navTabInfo, setNavTabInfo] = useState({ top: 0, height: 0 });
+    const handleScroll = (e: any) => {
+        if (e.detail.scrollTop >= navTabInfo.top) {
+            setNavTabFixed(true);
+        } else {
+            setNavTabFixed(false);
+        }
+    };
 
-  return (
-    <ScrollView
-      className="scrollview"
-      style={{ height: "100vh" }}
-      scrollY
-      scrollWithAnimation
-      lowerThreshold={50}
-      onScrollToLower={() => {
-        console.log("重新请求获取更多数据");
-      }}
-    >
-      {/* swiper */}
-      <Swiper
-        className="index-swiper bg-slate-50"
-        indicatorColor="#f8fafc"
-        indicatorActiveColor="#16a34a"
-        interval={2000}
-        circular
-        indicatorDots
-        autoplay
-      >
-        {[...Array(5)].map((_, i) => {
-          return (
-            <SwiperItem key={i}>
-              <View className="w-full h-full bg-slate-50 px-2 py-1">
-                <Image
-                  className="w-full h-full object-cover rounded-lg"
-                  src={`https://picsum.photos/id/${Random.integer(
-                    1,
-                    100
-                  )}/400/200`}
-                />
-              </View>
-            </SwiperItem>
-          );
-        })}
-      </Swiper>
+    useEffect(() => {
+        createSelectorQuery()
+            .select("#navTab")
+            .boundingClientRect()
+            .exec(function (res) {
+                console.log("@@@", res[0]);
+                setNavTabInfo(res[0]);
+            });
+    }, []);
 
-      {/* banner */}
-      <View className="h-32 w-full mt-4 mb-5 px-2 flex items-center justify-center shadow">
-        <Image
-          className="w-full h-full object-cover rounded-lg"
-          src={`https://picsum.photos/id/${Random.integer(1, 100)}/500/200`}
-        ></Image>
-      </View>
-
-      {/* sort */}
-      <View className="flex justify-between items-center mb-5 px-2">
-        <View className="text-xl font-light relative">
-          <View>无限卡牌</View>
-          <View className="absolute bottom-[-1] left-1/2 transform -translate-x-1/2 bg-green-600 w-4/5 h-1 rounded-lg"></View>
-        </View>
-        <View className="flex items-center gap-0.5" onClick={handleSort}>
-          <View>售价</View>
-          <View className="flex flex-col gap-0.5">
-            <View
-              className={`w-0 h-0 border-l-[8px] border-l-transparent border-r-[8px] border-r-transparent border-b-[10px] ${
-                sort === "asc" ? "border-b-green-600" : "border-b-black"
-              }`}
-            ></View>
-            <View
-              className={`w-0 h-0 border-l-[8px] border-l-transparent border-r-[8px] border-r-transparent border-t-[10px] ${
-                sort === "desc" ? "border-t-green-600" : "border-t-black"
-              }`}
-            ></View>
-          </View>
-        </View>
-      </View>
-
-      {/* card tabs */}
-      <View className="overflow-x-auto w-full mb-2 px-2">
-        <View className="grid grid-flow-col gap-2 auto-cols-max min-w-max">
-          {CARD_TYPE.map((type, index) => {
-            return (
-              <View
-                key={index}
-                className="text-xs py-2 px-3 rounded-2xl bg-white border border-gray-200 first:bg-gray-800 first:text-green-300"
-              >
-                {type}
-              </View>
-            );
-          })}
-        </View>
-      </View>
-
-      {/* card list */}
-      <View className="grid grid-cols-2 gap-3 px-2">
-        {[...Array(16)].map((_, i) => {
-          return (
-            <View
-              key={i}
-              className="border border-gray-200 rounded-lg overflow-hidden shadow"
+    return (
+        <ScrollView
+            className="scrollview"
+            style={{ height: "100vh" }}
+            scrollY
+            scrollWithAnimation
+            onScroll={handleScroll}
+            lowerThreshold={50}
+            onScrollToLower={() => {
+                console.log("重新请求获取更多数据");
+            }}
+        >
+            {/* swiper */}
+            <Swiper
+                className="index-swiper bg-slate-50"
+                indicatorColor="#f8fafc"
+                indicatorActiveColor="#16a34a"
+                interval={2000}
+                circular
+                indicatorDots
+                autoplay
             >
-              <View className="w-full h-44 relative">
-                <Image
-                  src={`https://picsum.photos/id/${Random.integer(1, 100)}/44`}
-                  className="w-full h-full object-cover"
-                ></Image>
-                <View className="absolute bottom-2 right-2 bg-gray-800/80 text-white text-xs rounded-lg px-2 py-1">
-                  {Random.integer(1, 20)}张/包
-                </View>
-              </View>
-              <View className="p-2">
-                <View className="truncate font-light">
-                  {Random.ctitle(4, 20)}
-                </View>
-                <View className="my-2">¥{Random.integer(1, 100)}</View>
-                <View className="text-xs text-gray-400">
-                  近{Random.integer(1, 24)}小时已抽 {Random.integer(1, 10)}{" "}
-                  万+张卡
-                </View>
-              </View>
-            </View>
-          );
-        })}
-      </View>
+                {[...Array(5)].map((_, i) => {
+                    return (
+                        <SwiperItem key={i}>
+                            <View className="w-full h-full bg-slate-50 px-2 py-1">
+                                <Image
+                                    className="w-full h-full object-cover rounded-lg"
+                                    src={`https://picsum.photos/id/${Random.integer(
+                                        1,
+                                        100
+                                    )}/400/200`}
+                                />
+                            </View>
+                        </SwiperItem>
+                    );
+                })}
+            </Swiper>
 
-      {/* load more and no more */}
-      <View className="flex justify-center items-center py-10">
-        <View className="text-gray-400 text-sm">没有更多了</View>
-      </View>
-    </ScrollView>
-  );
+            {/* banner */}
+            <View className="h-32 w-full mt-4 mb-5 px-2 flex items-center justify-center shadow">
+                <Image
+                    className="w-full h-full object-cover rounded-lg"
+                    src={`https://picsum.photos/id/${Random.integer(
+                        1,
+                        100
+                    )}/500/200`}
+                ></Image>
+            </View>
+
+            {/* sort */}
+            <View className="flex justify-between items-center mb-5 px-2">
+                <View className="text-xl relative">
+                    <View style={{ fontStyle: "italic" }}>无限卡牌</View>
+                    <View className="absolute bottom-[-1] left-1/2 transform -translate-x-1/2 bg-green-600 w-4/5 h-1 rounded-lg"></View>
+                </View>
+                <View
+                    className="flex items-center gap-0.5"
+                    onClick={handleSort}
+                >
+                    <View>售价</View>
+                    <View className="flex flex-col gap-0.5">
+                        <View
+                            className={`w-0 h-0 border-l-[8px] border-l-transparent border-r-[8px] border-r-transparent border-b-[10px] ${
+                                sort === "asc"
+                                    ? "border-b-green-600"
+                                    : "border-b-black"
+                            }`}
+                        ></View>
+                        <View
+                            className={`w-0 h-0 border-l-[8px] border-l-transparent border-r-[8px] border-r-transparent border-t-[10px] ${
+                                sort === "desc"
+                                    ? "border-t-green-600"
+                                    : "border-t-black"
+                            }`}
+                        ></View>
+                    </View>
+                </View>
+            </View>
+
+            {/* nav tabs */}
+            <View
+                id="navTab"
+                className={classnames("overflow-x-auto w-full px-2 py-2", {
+                    "fixed top-0 z-10 bg-slate-50 pb-3 shadow": navTabFixed,
+                })}
+            >
+                <View className="grid grid-flow-col gap-2 auto-cols-max min-w-max">
+                    {NAV_TABS.map((type, index) => {
+                        return (
+                            <View
+                                key={index}
+                                className="text-xs py-2 px-3 rounded-2xl bg-white border border-gray-200 first:bg-gray-800 first:text-green-300"
+                            >
+                                {type}
+                            </View>
+                        );
+                    })}
+                </View>
+            </View>
+
+            {/* card list */}
+            <View
+                className={classnames("grid grid-cols-2 gap-3 px-2")}
+                style={{
+                    paddingTop: navTabFixed ? `${navTabInfo.height + 2}px` : 0,
+                }}
+            >
+                {[...Array(16)].map((_, i) => {
+                    return (
+                        <View
+                            key={i}
+                            className="border border-gray-200 rounded-lg overflow-hidden shadow"
+                        >
+                            <View className="w-full h-44 relative">
+                                <Image
+                                    src={`https://picsum.photos/id/${Random.integer(
+                                        1,
+                                        100
+                                    )}/44`}
+                                    className="w-full h-full object-cover"
+                                ></Image>
+                                <View className="absolute bottom-2 right-2 bg-gray-800/80 text-white text-xs rounded-lg px-2 py-1">
+                                    {Random.integer(1, 20)}张/包
+                                </View>
+                            </View>
+                            <View className="p-2">
+                                <View className="truncate font-light">
+                                    {Random.ctitle(4, 20)}
+                                </View>
+                                <View className="my-2">
+                                    ¥{Random.integer(1, 100)}
+                                </View>
+                                <View className="text-xs text-gray-400">
+                                    近{Random.integer(1, 24)}小时已抽{" "}
+                                    {Random.integer(1, 10)} 万+张卡
+                                </View>
+                            </View>
+                        </View>
+                    );
+                })}
+            </View>
+
+            {/* load more and no more */}
+            <View className="flex justify-center items-center py-10">
+                <View className="text-gray-400 text-sm">没有更多了</View>
+            </View>
+        </ScrollView>
+    );
 }
